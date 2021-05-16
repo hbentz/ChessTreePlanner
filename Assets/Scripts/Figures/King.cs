f208c1a5e04d5667eca920e369818e2c14606eb8
hbentz <hbentz@ualberta.ca>
2021-05-16 16:08:46 -0700

    Add castling rules

--- Assets/Scripts/Figures/King.cs
+++ Assets/Scripts/Figures/King.cs
@@ -8,6 +8,33 @@ public class King : ChessFigure
 
     public override bool[,] PossibleMove()
     {
+        bool[,] moves = PossibleAttacks();
+        ChessTile[,] tiles = Tile.Board.Tiles;
+
+        // Castle legality check
+        if (!HasMoved)  // If the king hasn't moved
+        {
+            int castleY = isBlack ? 7 : 0; // Figure out which rank the castle happens on
+
+            // If the queenside rook also hasn't moved, the spaces are empty, and the king is not under threat while travelling
+            bool canQueensideCastle = tiles[0, castleY].HasFriendlyPiece(isBlack) && !tiles[0, castleY].Figure.HasMoved;
+            for (int i = 1; i < 4; i++) canQueensideCastle = canQueensideCastle && !tiles[i, castleY].HasPiece();
+            for (int i = 2; i <= 4; i++) canQueensideCastle = canQueensideCastle && tiles[i, castleY].ThreatenedBy().Count == 0;
+            moves[2, castleY] = canQueensideCastle;
+
+            // If the kingside rook also hasn't moved, the spaces are empty, and the king is not under threat while travelling
+            bool canKingsideCastle = tiles[7, castleY].HasFriendlyPiece(isBlack) && !tiles[7, castleY].Figure.HasMoved;
+            for (int i = 5; i < 7; i++) canKingsideCastle = canKingsideCastle && !tiles[i, castleY].HasPiece();
+            for (int i = 4; i < 7; i++) canKingsideCastle = canKingsideCastle && tiles[i, castleY].ThreatenedBy().Count == 0;
+            moves[6, castleY] = canKingsideCastle;
+        }
+
+        // TODO: Eliminate moves that put the king in check
+        return moves;
+    }
+
+    public override bool[,] PossibleAttacks()
+    {
         bool[,] moves = new bool[8, 8];
         int currX = Tile.xCoord;
         int currY = Tile.yCoord;
@@ -16,16 +43,27 @@ public class King : ChessFigure
         AddMoveIfOnboardAndNotConflicting(currX + 1, currY, ref tiles, ref moves);
         AddMoveIfOnboardAndNotConflicting(currX - 1, currY, ref tiles, ref moves);
         AddMoveIfOnboardAndNotConflicting(currX + 1, currY + 1, ref tiles, ref moves);
-        AddMoveIfOnboardAndNotConflicting(currX + 1, currY -1, ref tiles, ref moves);
+        AddMoveIfOnboardAndNotConflicting(currX + 1, currY - 1, ref tiles, ref moves);
         AddMoveIfOnboardAndNotConflicting(currX - 1, currY + 1, ref tiles, ref moves);
         AddMoveIfOnboardAndNotConflicting(currX - 1, currY - 1, ref tiles, ref moves);
         AddMoveIfOnboardAndNotConflicting(currX, currY + 1, ref tiles, ref moves);
         AddMoveIfOnboardAndNotConflicting(currX, currY - 1, ref tiles, ref moves);
 
-        // TODO: Check for moves that put the king in danger
-        // TODO: Castling
-
         return moves;
+    }
+
+    public override void SetPosition(ChessTile tile)
+    {
+        if (!HasMoved) // If this is the first move
+        {
+            int castleY = isBlack ? 7 : 0;
+            ChessTile[,] tiles = tile.Board.Tiles;
+
+            // Move the Queenside rook or kingside rook if castling
+            if (tile.xCoord == 2) tiles[0, castleY].Figure.SetPosition(tiles[3, castleY]);
+            else if (tile.xCoord == 6) tiles[7, castleY].Figure.SetPosition(tiles[5, castleY]);
+        }
+        base.SetPosition(tile);
     }
 }
 
