fdb9bce4068a49bb20aae2443d7a3d4e78cd9098
hbentz <hbentz@ualberta.ca>
2021-05-15 19:18:03 -0700

    Bishop move logic

--- Assets/Scripts/Figures/Bishop.cs
+++ Assets/Scripts/Figures/Bishop.cs
@@ -7,7 +7,71 @@ public class Bishop : ChessFigure
     // Start is called before the first frame update
     public override bool[,] PossibleMove()
     {
-        return new bool[8, 8];
+        bool[,] moves = new bool[8, 8];
+
+        int currX = Tile.xCoord;
+        int currY = Tile.yCoord;
+
+        ChessTile[,] tiles = Tile.Board.Tiles;
+        ChessTile tile;
+
+        // Crawl through each of the four diagonals 
+        // Stopping on any piece but noting a possible move if the unit is of a different color
+
+        // Crawl up the top left diagonal
+        int j;
+        for (int i = currX - 1; i >= 0; i--)
+        {
+            j = currY + (currX - i);
+            if (j >= 8) break;
+            if (tiles[i, j].Figure == null) moves[i, j] = true;
+            else
+            {
+                if (isBlack != tiles[i, j].Figure.isBlack) moves[i, j] = true;
+                break;
+            }
+        }
+
+        // Crawl up the top right diagonal
+        for (int i = currX + 1; i < 8; i++)
+        {
+            j = currY + (i - currX);
+            if (j >= 8) break;
+            if (tiles[i, j].Figure == null) moves[i, j] = true;
+            else
+            {
+                if (isBlack != tiles[i, j].Figure.isBlack) moves[i, j] = true;
+                break;
+            }
+        }
+
+        // Crawl down the bottom right diagonal
+        for (int i = currX + 1; i < 8; i++)
+        {
+            j = currY - (i - currX);
+            if (j < 0) break;
+            if (tiles[i, j].Figure == null) moves[i, j] = true;
+            else
+            {
+                if (isBlack != tiles[i, j].Figure.isBlack) moves[i, j] = true;
+                break;
+            }
+        }
+
+        // Crawl down the bottom left diagonal
+        for (int i = currX - 1; i >= 0; i--)
+        {
+            j = currY - (currX - i);
+            if (j < 0) break;
+            if (tiles[i, j].Figure == null) moves[i, j] = true;
+            else
+            {
+                if (isBlack != tiles[i, j].Figure.isBlack) moves[i, j] = true;
+                break;
+            }
+        }
+
+        return moves;
     }
 }
 
